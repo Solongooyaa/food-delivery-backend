3d2e355248da1860daa91b933c6cd9c0bcd46f24
24LP8639 24LP8639 <24LP8639@G305-employeeiMac.local>
2025-01-13 18:13:44 +0800

    first commit

---
+++ index.ts
@@ -0,0 +1,22 @@
+import { configDotenv } from "dotenv";
+import express, { Request, Response } from "express";
+const mongoose = require("mongoose");
+
+const PORT = 8000;
+const app = express();
+app.use(express.json());
+
+configDotenv();
+const URI = process.env.MONGODB_URI;
+console.log(URI);
+export const connectMongoDB = async () => {
+  const MONGODB_URI = process.env.MONGODB_URI;
+};
+
+// app.use(cors());
+app.get("/", async (req: Request, res: Response) => {
+  res.send("Hello");
+});
+app.listen(PORT, () => {
+  console.log(`Server is Running on http://localhost:${PORT}`);
+});
