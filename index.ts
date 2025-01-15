d62195a21de80ec3167e94aef01e37f96f9b117c
Solongooyaa <u.solongo2018@gmail.com>
2025-01-14 21:07:59 -0800

    backend

--- index.ts
+++ index.ts
@@ -1,27 +1,18 @@
 import { configDotenv } from "dotenv";
 import express, { Request, Response } from "express";
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const PORT = 8000;
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 configDotenv();
-const URI = process.env.MONGODB_URI;
 
 const connectMongoDB = async () => {
   const MONGODB_URI = process.env.MONGODB_URI;
-
-  if (!MONGODB_URI) {
-    throw new Error(
-      "Database Connection URI is not definedd in environment variables."
-    );
-  }
-  try {
-    await mongoose.connect(MONGODB_URI);
-  } catch (error) {
-    console.log("Failed to connect to the MongoDB database:", error);
-  }
+  await mongoose.connect(MONGODB_URI);
 };
 
 connectMongoDB();
@@ -36,54 +27,41 @@ const FoodCategoryModel = mongoose.model(
   FOOD_CATEGORY_SCHEMA,
   "food-category"
 );
+
 app.get("/food-category/", async (req: Request, res: Response) => {
   //Create new Food Category
   const data = await FoodCategoryModel.find();
-  res.send(data);
+  res.json(data);
 });
+
 app.get("/food-category/:id", async (req: Request, res: Response) => {
   //Create Food Category
-  const id = req.params;
-  //   const date = Date.now();
-
-  const newItem = await FoodCategoryModel.findById({ id });
-  res.send({
-    message: "New Food.",
-    newItem,
-  });
+  const id = req.params.id;
+  const item = await FoodCategoryModel.findById(id);
+  res.json(item);
 });
 
-app.post("/food-category/:id", async (req: Request, res: Response) => {
-  //Create new Food Category
-
-  const newItem = await FoodCategoryModel.create({ categoryName: "Fruit" });
-  res.send({
-    message: "New Food.",
-    newItem,
+app.post("/food-category/", async (req: Request, res: Response) => {
+  const newItem = await FoodCategoryModel.create({
+    categoryName: req.body.categoryName,
   });
+  res.json(newItem);
 });
 
 app.put("/food-category/:id", async (req: Request, res: Response) => {
-  // Update Food Category
-  const updateItem = await FoodCategoryModel.create({
-    categoryName: req.body.categoryName,
-  });
-
-  //Fetch all Food Categories
-  const foodCategories = await FoodCategoryModel.find();
-  res.send({
-    message: "hi",
-    foodCategories,
-  });
-
-  console.log(foodCategories);
-  //   console.log(id);
+  const updatedItem = await FoodCategoryModel.findByIdAndUpdate(
+    req.params.id,
+    {
+      categoryName: req.body.categoryName,
+    },
+    { new: true }
+  );
+  res.json(updatedItem);
 });
+
 app.delete("/food-category/:id", async (req: Request, res: Response) => {
-  //Delete all Food Categories
-  // const foodCategories = await FoodCategoryModel.filter(());
-  const deleteItem = await FoodCategoryModel.findByDelete(req.params.id);
-  res.json(deleteItem);
+  const deletedItem = await FoodCategoryModel.findByIdAndDelete(req.params.id);
+  res.json(deletedItem);
 });
 
 app.listen(PORT, () => {
