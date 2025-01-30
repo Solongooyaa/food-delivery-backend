a40987e42899483db7f3ee53230f4d01cd9b5ab3
24LP8639 24LP8639 <24LP8639@G305-employeeiMac.local>
2025-01-30 18:21:40 +0800

    backend

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,20 @@
-// export const middleware = () => {
+// export const auth = async (req: any, res: any, next: any) => {
+//   const token = req.get("authentication");
 
-//     return (
-
-//     )
+//   try {
+//     const verified = await verifyToken(token, {
+//       secretKey: process.env.CLERK_SECRET_KEY,
+//     });
+//     console.log({ verified });
+//     const userId = verified?.sub;
+//     console.log(userId);
+//     req.userId = userId;
+//     next();
+//   } catch {
+//     res.json({ status: "Forbidden" });
+//   }
 // };
+
+// function verifyToken(token: any, arg1: { secretKey: string | undefined }) {
+//     throw new Error("Function not implemented.");
+//   }
--- router/food-order.ts
+++ router/food-order.ts
@@ -3,22 +3,6 @@ import { FoodOrderModel } from "../models/food-order";
 // import { CustomRequest } from "../constants/type";
 
 export const foodOrderRouter = Router();
-// const auth = async (req: any, res: any, next: any) => {
-//   const token = req.get("authentication");
-
-//   try {
-//     const verified = await verifyToken(token, {
-//       secretKey: process.env.CLERK_SECRET_KEY,
-//     });
-// console.log({ verified });
-// const userId = verified?.sub;
-// console.log(userId);
-// req.userId = userId;
-//     next();
-//   } catch {
-//     res.json({ status: "Forbidden" });
-//   }
-// };
 
 foodOrderRouter.get("/", async (req: Request, res: Response) => {
   const allFoodOrder = await FoodOrderModel.find({});
@@ -61,6 +45,3 @@ foodOrderRouter.delete("/:id", async (req: Request, res: Response) => {
   );
   res.json(deletedFoodOrder);
 });
-function verifyToken(token: any, arg1: { secretKey: string | undefined }) {
-  throw new Error("Function not implemented.");
-}
